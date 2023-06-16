3343881b8154f78a4531156dbad570e56b1aebcb
Gustavo Soldera <gustavosevere@gmail.com>
2023-06-16 08:27:25 -0300

    feat: card component

---
+++ src/components/namecard/Card.jsx
@@ -0,0 +1,18 @@
+import "./Card.scss"
+import PropTypes from "prop-types"
+
+Card.propTypes = {
+  name: PropTypes.string,
+  time: PropTypes.string.isRequired,
+}
+
+export default function Card(props) {
+  return (
+    <>
+      <div className="card">
+        <strong>{props.name ? props.name : "New Member"}</strong>
+        <small>{props.time}</small>
+      </div>
+    </>
+  )
+}
